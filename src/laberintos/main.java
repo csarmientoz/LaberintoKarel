2256395e4b44532384be82c16375b0dfe9803eb7
Cristian Sarmiento <Cristian Sarmiento@CRISTIAN>
2017-03-15 12:18:11 -0500

    Robot apto para salir de cualquier tipo de laberinto

---
+++ src/laberintos/main.java
@@ -0,0 +1,15 @@
+
+package laberintos;
+
+
+
+public class main {
+
+     public static void main(String[] args) {
+ 
+    
+     Laberinto laberintos = new Laberinto();   
+     laberintos.laberinto();
+     }
+    
+}
